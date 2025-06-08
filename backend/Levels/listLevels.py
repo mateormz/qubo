c8094f26be2f4c48bad1e14904d20366b3a49dd5
joel <joel.miguel@utec.edu.pe>
2025-06-08 01:15:33 -0500

    avance7

--- backend/Levels/listLevels.py
+++ backend/Levels/listLevels.py
@@ -2,7 +2,7 @@ import json
 import os
 import boto3
 from boto3.dynamodb.conditions import Key
-from common import validate_token
+from common import validate_token, convert_decimal
 
 dynamodb = boto3.resource('dynamodb')
 lambda_client = boto3.client('lambda')
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
 
         return {
             'statusCode': 200,
-            'body': json.dumps({'levels': levels})
+            'body': json.dumps({'levels': convert_decimal(levels)})
         }
 
     except Exception as e:
