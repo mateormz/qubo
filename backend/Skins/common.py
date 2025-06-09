8d90c247bf70c07e579c45c28b5cd1949ab336e5
Mateo <mateo.ramirez@utec.edu.pe>
2025-06-09 00:12:28 -0500

    Skins API

---
+++ backend/Skins/common.py
@@ -0,0 +1,26 @@
+import json
+import os
+import boto3
+from common import convert_decimal
+
+dynamodb = boto3.resource('dynamodb')
+
+def lambda_handler(event, context):
+    try:
+        table = dynamodb.Table(os.environ['SKINS_TABLE'])
+
+        response = table.scan()
+        items = response.get('Items', [])
+
+        return {
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps(convert_decimal(items))
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps({'error': 'Internal Server Error', 'details': str(e)})
+        }
