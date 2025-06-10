11330d3322484d6089f9874d6dad7ed3d9781248
joel <joel.miguel@utec.edu.pe>
2025-06-10 13:37:07 -0500

    Completado

--- backend/User/getUserById.py
+++ backend/User/getUserById.py
@@ -9,10 +9,12 @@ lambda_client = boto3.client('lambda')
 
 def lambda_handler(event, context):
     try:
+        # Validar token
         user_info = validate_token(event, lambda_client)
         if 'statusCode' in user_info:
             return user_info
 
+        # Verificar la propiedad del usuario
         error = ensure_user_ownership(event, user_info['user_id'])
         if error:
             return error
@@ -22,6 +24,7 @@ def lambda_handler(event, context):
 
         user_id = event['pathParameters']['user_id']
 
+        # Obtener la información del usuario de DynamoDB
         response = user_table.get_item(Key={'user_id': user_id})
         item = response.get('Item')
 
@@ -32,21 +35,34 @@ def lambda_handler(event, context):
                 'body': json.dumps({'error': 'User not found'})
             }
 
+        # Eliminar la contraseña antes de retornar
         item.pop('password_hash', None)
 
+        # Obtener el rol del usuario desde la base de datos
+        user_role = item.get('role', None)  # Asegúrate de que el rol esté presente en el item
+        if user_role != 'student':  # Solo proceder si el usuario tiene el rol de estudiante
+            return {
+                'statusCode': 200,
+                'headers': {'Content-Type': 'application/json'},
+                'body': json.dumps(convert_decimal(item))
+            }
+
+        # Si el rol es "student", proceder a asignar los juegos en el nivel 1
         level_progress = item.get('levelProgress', {})
         updated = False
 
-        # Obtener todos los game_id existentes
+        # Obtener todos los game_id existentes desde la tabla de juegos
         all_games = games_table.scan().get('Items', [])
         for game in all_games:
             game_id = game['game_id']
+            # Asignar el juego solo si no está presente en levelProgress
             if game_id not in level_progress:
-                level_progress[game_id] = 1
+                level_progress[game_id] = 1  # Asignar nivel 1 al juego
                 updated = True
 
         if updated:
             item['levelProgress'] = level_progress
+            # Actualizar la tabla de usuarios con el nuevo progreso de nivel
             user_table.update_item(
                 Key={'user_id': user_id},
                 UpdateExpression='SET levelProgress = :lp',
